583756eda267c135c90be302029877c4e354b0f4
Krzysztof Huczek <krzysztof.huczek@gmail.com>
2021-01-30 16:08:07 +0100

    init commit - basic project structure - done, buttons controller - done

---
+++ src/sample/controller/Buttons.java
@@ -0,0 +1,40 @@
+package sample.controller;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Buttons {
+    public void handleLogBtn(ActionEvent actionEvent) {
+        try{
+            changeRoot((Node) actionEvent.getSource(),"../view/invoiceLog.fxml");
+        }catch(Exception e) {
+            System.out.println(e.getStackTrace());
+        }
+    }
+    public void handleAddBtn(ActionEvent actionEvent) {
+        try{
+            changeRoot((Node) actionEvent.getSource(),"../view/invoiceNew.fxml");
+        }catch(Exception e){
+            System.out.println(e.getStackTrace());
+        }
+    }
+    public void handleSettingsBtn(ActionEvent actionEvent) {
+        try{
+            changeRoot((Node) actionEvent.getSource(),"../view/invoiceSettings.fxml");
+        }catch(Exception e){
+            System.out.println(e.getStackTrace());
+        }
+    }
+
+    private void changeRoot(Node source, String path) throws IOException {
+        Parent newRoot = FXMLLoader.load(getClass().getResource(path));
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.getScene().setRoot(newRoot);
+    }
+}
---
+++ src/sample/controller/Log.java
@@ -0,0 +1,14 @@
+package sample.controller;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Log {
+
+}
